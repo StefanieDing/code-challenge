442c456fae20358a580d8804b0e6be06e971ba06
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-06-02 20:36:48 -0400

    added category and list db

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,7 +8,25 @@ var List = require('../models')['List'];
 List.sync();
 
 router.get('/', function(req, res){
-  res.render('index');
+  //grab categories and list from db
+  var info={
+    category: [],
+    list: []
+  };
+
+  Category.findAll({
+    attributes: ['id', 'title', 'description', 'icon']
+  }).then(function(data){
+      info.category = data;
+
+      List.findAll({
+        attributes: ['id', 'title', 'description', 'type', 'icon']
+      }).then(function(result){
+        info.list = result;
+          res.render('index', info);
+      });
+  });
 });
+
 
 module.exports = router;
