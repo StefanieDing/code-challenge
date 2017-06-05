8182a214ffdbd03cfc0aff86aa89a252a64cc86c
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-06-05 14:54:59 -0400

    added routes to controller.js

--- migrations/20170602213236-create-category.js
+++ migrations/20170602213236-create-category.js
@@ -19,11 +19,13 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        default: sequelize.literal(CURRENT_TIMESTAMP)
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        default: sequelize.literal(CURRENT_TIMESTAMP)
       }
     });
   },
